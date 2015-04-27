18f1a2dba3244de6eb8cffc6d8791ce90cf639b2
HarigovindV <itshari4ever@gmail.com>
2015-04-27 22:17:00 +0530

    Update Cell.h

--- Cell.h
+++ Cell.h
@@ -1,17 +1,20 @@
 #ifndef BSIM_CELL_H
 #define BSIM_CELL_H
 #include <cstdint>
+typedef std::int8_t Voltage
+typedef std::int8_t InternalResistance
+typedef Simulator::CircuitElement::NON_IDEAL_DC_VOLTAGE_SOURCE BatteryCell
 namespace Simulator{
   namespace CircuitElement{
 struct NON_IDEAL_DC_VOLTAGE_SOURCE{
 NON_IDEAL_DC_VOLTAGE_SOURCE();
-Set_Potential();
-Get_Potential();
-Set_internal_Resistance();
-Get_internal_Resistance();
+Set_PotentialVoltage(Voltage);
+Voltage Get_Potential(void);
+Set_internal_Resistance(InternalResistance);
+InternalResistance Get_internal_Resistance(void);
 private:
-std::uint8_t Potential_in_Volts;
-std::uint8_t InternalResistance_in_mOhm;
+Voltage Potential_in_Volts;
+InternalResistance InternalResistance_in_mOhm;
 }//DC_VOLTAGE_SOURCE
   }//CircuitElement
 }//Simulator

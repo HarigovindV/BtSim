3db99a06a7bab804bc6f55b6bb21debe9c3130ed
Harigovind <itshari4ever@gmail.com>
2015-05-08 15:13:32 +0530

    Updated Maximum BatteryCharge
    Updated CellCapacity for Faster Discharge

--- GlobalDeclarations.h
+++ GlobalDeclarations.h
@@ -5,7 +5,7 @@ namespace Simulator{
 	namespace Globals{
 static const Voltage MaximumCellVoltage=12000;
 static const Voltage MinimumCellVoltage=7500;
-static const BatteryCapacity MaximumCellCapacity=4500;
+static const BatteryCapacity MaximumCellCapacity=1000;
 static const BatteryCapacity MinimumCellCapacity=0;
 static const double VoltageDropPermAh=(MaximumCellVoltage-MinimumCellVoltage)/(MaximumCellCapacity-MinimumCellCapacity);
 static const InternalResistance DefaultInternalResistanceValueInmAh=0;
